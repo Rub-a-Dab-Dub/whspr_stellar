de5809473049ae1c0d974b9f37d2d61c1530f982
mubarak23 <mubarakaminu340@gmail.com>
2026-01-25 23:21:06 +0100

    feat:register a new user

--- contracts/whsper_stellar/src/lib.rs
+++ contracts/whsper_stellar/src/lib.rs
@@ -18,6 +18,8 @@ pub struct BaseContract;
 pub enum DataKey {
     Admin,
     Metadata,
+    User(Address),
+    Username(Symbol),
 }
 
 /// Simple metadata structure
@@ -27,6 +29,18 @@ pub struct ContractMetadata {
     pub name: Symbol,
     pub version: u32,
 }
+
+#[derive(Clone)]
+#[contracttype]
+pub struct UserProfile {
+    pub address: Address,
+    pub username: Symbol,
+    pub xp: u64,
+    pub level: u32,
+    pub badges: Vec<Symbol>,
+    pub join_date: u64,
+}
+
 
 
 #[contractimpl]
@@ -57,5 +71,67 @@ impl BaseContract {
             .get(&DataKey::Metadata)
             .expect("not initialized")
     }
+
+     pub fn register_user(env: Env, username: Symbol) {
+        let caller = env.invoker();
+        caller.require_auth();
+
+        // validate username length
+        validate_username(&username);
+
+        if env.storage().instance().has(&DataKey::User(caller.clone())) {
+            panic!("user already registered"); // use custom error 
+        }
+
+         if env.storage().instance().has(&DataKey::Username(username.clone())) {
+            panic!("username already taken");
+        }
+
+        let join_date = env.ledger().timestamp();
+        
+        let profile = UserProfile {
+            address: caller.clone(),
+            username: username.clone(),
+            xp: 0,
+            level: 1,
+            badges: Vec::new(&env),
+            join_date,
+        };
+
+        env.storage()
+            .instance()
+            .set(&DataKey::User(caller.clone()), &profile);
+
+        env.storage()
+            .instance()
+            .set(&DataKey::Username(username), &caller);
+
+     }
+
+    pub fn get_user_profile(env: Env, user: Address) -> UserProfile {
+    env.storage()
+        .instance()
+        .get(&DataKey::User(user))
+        .expect("user not found")
+    }
+
 }
 
+
+fn validate_username(username: &Symbol) {
+    let len = username.len();
+    if len < 3 || len > 16 {
+        panic!("invalid username length");
+    }
+}
+
+// subject to change base on xp point design
+fn calculate_level(xp: u64) -> u32 {
+    match xp {
+        0..=99 => 1,
+        100..=299 => 2,
+        300..=599 => 3,
+        600..=999 => 4,
+        _ => 5,
+    }
+}
