8236322d05e3df9782c936967bb77f35f21da12d
Emeka <Kingsleyokoli71@gmail.com>
2026-01-26 17:55:31 +0100

    Target rooms

---
+++ src/Targeted-rooms/rooms.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ScheduleModule } from '@nestjs/schedule';
+import { Room } from './entities/room.entity';
+import { RoomsService } from './rooms.service';
+import { RoomsController } from './rooms.controller';
+import { RoomCleanupTask } from './tasks/room-cleanup.task';
+import { RoomEventsListener } from './listeners/room-events.listener';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Room]),
+    ScheduleModule.forRoot(),
+  ],
+  controllers: [RoomsController],
+  providers: [RoomsService, RoomCleanupTask, RoomEventsListener],
+  exports: [RoomsService],
+})
+export class RoomsModule {}
