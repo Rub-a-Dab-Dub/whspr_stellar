c17f889ee6656185e694896d7a70ed2f7ad12d60
Ajidokwu Sabo <realjaiboi70@gmail.com>
2026-02-20 00:11:12 +0100

    Real-time platform activity feed via WebSocket

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ScheduleModule } from '@nestjs/schedule';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import { User } from '../user/entities/user.entity';
@@ -13,10 +15,13 @@ import { AuditLogRetentionJob } from './jobs/audit-log-retention.job';
 import { Transfer } from '../transfer/entities/transfer.entity';
 import { Session } from '../sessions/entities/session.entity';
 import { Message } from '../message/entities/message.entity';
+import { AdminEventStreamGateway } from './gateways/admin-event-stream.gateway';
 
 @Module({
   imports: [
     ScheduleModule.forRoot(),
+    ConfigModule,
+    JwtModule.register({}),
     TypeOrmModule.forFeature([
       User,
       AuditLog,
@@ -29,7 +34,12 @@ import { Message } from '../message/entities/message.entity';
     ]),
   ],
   controllers: [AdminController],
-  providers: [AdminService, AuditLogService, AuditLogRetentionJob],
+  providers: [
+    AdminService,
+    AuditLogService,
+    AuditLogRetentionJob,
+    AdminEventStreamGateway,
+  ],
   exports: [AdminService, AuditLogService],
 })
 export class AdminModule {}
