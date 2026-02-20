0afc57ce2125d814e8fbda4235cfe3c62d430314
abdegenius <johnnieabijah2000@gmail.com>
2026-02-20 14:04:30 +0100

    updates

---
+++ src/admin/entities/platform-config.entity.ts
@@ -0,0 +1,31 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  UpdateDateColumn,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity('platform_configs')
+export class PlatformConfig {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  key: string;
+
+  @Column({ type: 'jsonb' })
+  value: any;
+
+  @Column({ type: 'text', nullable: true })
+  description: string;
+
+  @Column({ type: 'uuid', nullable: true })
+  updatedBy: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
